f25421a2aac880787d0c6207e236b160304a915f
morteza <kashani.morteza@gmail.com>
2026-09-11 21:16:45 +0000

    clear

--- my_model/src/my_model/_base.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel as _PydanticBaseModel
-from pydantic import ConfigDict
-
-
-class BaseModel(_PydanticBaseModel):
-    model_config = ConfigDict(
-        extra="forbid",
-        validate_assignment=True,
-    )
